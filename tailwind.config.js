bd55cd2f297c3476a786b1faec782a0c3ecc5174
zubin-madon <madon.zubin@gmail.com>
2022-03-08 11:17:34 +0530

    test commit for stupid netlify piece of crap

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+      'spot-yellow': '#fff600',
+    }
+  },
+  },
+  plugins: [],
+}
